f1c04643aa49aa9754e2ab8459fac8e941e6b21d
iUsup <yusub1988@gmail.com>
2024-12-19 10:42:01 +0300

    fix: Hiber config file done

--- src/main/java/org/example/app/config/HibernateConfig.java
+++ src/main/java/org/example/app/config/HibernateConfig.java
@@ -5,18 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 import javax.sql.DataSource;
-import javax.annotation.Resource;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 import java.util.Properties;
 
 @Configuration
@@ -25,13 +22,12 @@ import java.util.Properties;
 @PropertySource("classpath:db.properties")
 @ComponentScan("org.example.app")
 public class HibernateConfig {
-    @Resource
-    private Environment env;
+    private final Environment env;
 
-//    @Autowired
-//    public void setEnv(Environment env) {
-//        this.env = env;
-//    }
+    @Autowired
+    public HibernateConfig(Environment env) {
+        this.env = env;
+    }
 
     @Bean
     public DataSource getDataSource() {
@@ -41,12 +37,12 @@ public class HibernateConfig {
         dataSource.setUsername(env.getProperty("db.username"));
         dataSource.setPassword(env.getProperty("db.password"));
 
-        dataSource.setInitialSize(Integer.valueOf(env.getProperty("db.initialSize")));
-        dataSource.setMinIdle(Integer.valueOf(env.getProperty("db.minIdle")));
-        dataSource.setMaxIdle(Integer.valueOf(env.getProperty("db.maxIdle")));
-        dataSource.setTimeBetweenEvictionRunsMillis(Integer.valueOf(env.getProperty("db.evictionTimeMilli")));
-        dataSource.setMinEvictableIdleTimeMillis(Integer.valueOf(env.getProperty("db.minEvictableIdleTimeMillis")));
-        dataSource.setTestOnBorrow(Boolean.valueOf(env.getProperty("db.testOnBorrow")));
+        dataSource.setInitialSize(Integer.parseInt(Objects.requireNonNull(env.getProperty("db.initialize"))));
+        dataSource.setMinIdle(Integer.parseInt(Objects.requireNonNull(env.getProperty("db.minIdle"))));
+        dataSource.setMaxIdle(Integer.parseInt(Objects.requireNonNull(env.getProperty("db.maxIdle"))));
+        dataSource.setTimeBetweenEvictionRunsMillis(Integer.parseInt(Objects.requireNonNull(env.getProperty("db.timeBetweenEvictionRunsMillis"))));
+        dataSource.setMinEvictableIdleTimeMillis(Integer.parseInt(Objects.requireNonNull(env.getProperty("db.minEvictableIdleTimeMillis"))));
+        dataSource.setTestOnBorrow(Boolean.parseBoolean(env.getProperty("db.testOnBorrow")));
         dataSource.setValidationQuery(env.getProperty("db.validationQuery"));
         return dataSource;
     }
@@ -71,27 +67,6 @@ public class HibernateConfig {
             throw new RuntimeException(e);
         }
     }
-
-//    @Bean
-//    public LocalSessionFactoryBean getSessionFactory() {
-//        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
-//        factoryBean.setDataSource(getDataSource());
-//
-//        Properties props = new Properties();
-//        props.put("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-//        props.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
-//
-//        factoryBean.setHibernateProperties(props);
-//        factoryBean.setPackagesToScan("hiber.model");
-//        return factoryBean;
-//    }
-
-//    @Bean
-//    public HibernateTransactionManager getTransactionManager() {
-//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-//        transactionManager.setSessionFactory(getSessionFactory().getObject());
-//        return transactionManager;
-//    }
 
     @Bean
     public PlatformTransactionManager getTransactionManager2() {
